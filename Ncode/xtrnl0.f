0e4161c4cccb4cbb3536b5181398f16686e45874
Florent <florent@r2d2.local>
2014-10-27 19:55:59 +0100

    xtrnl0 corrected

--- Ncode/xtrnl0.f
+++ Ncode/xtrnl0.f
@@ -242,10 +242,7 @@
       END IF
 *
 *       Include Plummer potential for 2D and 3D (use MP = 0 if not needed).
-*** FlorentR - Tidal tensor
-      IF (KZ(14).EQ.3.OR.KZ(14).EQ.4.OR.KZ(14).EQ.9) THEN
-*      IF (KZ(14).EQ.3.OR.KZ(14).EQ.4) THEN
-*** FRenaud
+      IF (KZ(14).EQ.3.OR.KZ(14).EQ.4) THEN
 *       Check input for Plummer potential.
           READ (5,*)  MP, AP2, MPDOT, TDELAY
           WRITE (6,70)  MP, AP2, MPDOT, TDELAY
